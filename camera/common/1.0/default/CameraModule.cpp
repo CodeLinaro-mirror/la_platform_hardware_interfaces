5ad61dca670de8ea768729580533d5997b56a370
Jasmine Chen <lnishan@google.com>
2021-02-25 21:57:31 +0800

    Camera: Fix double-free in removeCamera
    When a camera is removed, |hidlMetadata| takes over ownership of
    |metadata|. Therefore, we should not free |metadata| again.
    
    Bug: 180014486
    Test: Remove an external camera, and no crashes were observed.
    (cherry picked from commit ecd3296f1eb58e250b46b28761b13f88dcb025f6)
    
    Change-Id: I20292cec8c34cbfb6775118832ef8e20e95bae5a

--- camera/common/1.0/default/CameraModule.cpp
+++ camera/common/1.0/default/CameraModule.cpp
@@ -573,7 +573,6 @@ void CameraModule::removeCamera(int cameraId) {
                 }
             }
         }
-        free_camera_metadata(metadata);
     }
 
     mCameraInfoMap.removeItem(cameraId);
