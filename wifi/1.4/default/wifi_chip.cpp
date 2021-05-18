478751927c2cd438a5f3072ff26435763149fa25
Chenming Huang <chenhuan@codeaurora.org>
2021-05-18 15:02:11 +0800

    wifi: rename ap iface name
    As AP+AP+STA+STA feature developed, we cannot tell
    whether a iface, e.g. "wlan2" is a STA iface or a AP
    iface. Customer request to change the situation to
    avoid some firewall issues.
    
    Here we change AP iface name to "swlan0", "swlan1"
    while STA iface unchanged as "wlan0", "wlan1".
    
    CRs-Fixed: 2947960
    Change-Id: Ie283242e253270c6e75007d5f6456c8e2359f720

--- wifi/1.4/default/wifi_chip.cpp
+++ wifi/1.4/default/wifi_chip.cpp
@@ -45,7 +45,7 @@ constexpr char kTombstoneFolderPath[] = "/data/vendor/tombstones/wifi/";
 constexpr char kActiveWlanIfaceNameProperty[] = "wifi.active.interface";
 constexpr char kWlanDualBandProperty[] = "wifi.softap.bands";
 constexpr char kNoActiveWlanIfaceNamePropertyValue[] = "";
-constexpr unsigned kMaxWlanIfaces = 5;
+constexpr unsigned kMaxWlanIfaces = 3;
 constexpr char kBridgeIfacePrefix[] = "wifi_br";
 
 template <typename Iface>
@@ -102,16 +102,6 @@ std::string getWlanIfaceName(unsigned idx) {
     if (res > 0) return buffer.data();
 
     return "wlan" + std::to_string(idx);
-}
-
-// Returns the dedicated iface name if one is defined.
-std::string getApIfaceName() {
-    std::array<char, PROPERTY_VALUE_MAX> buffer;
-    if (property_get("ro.vendor.wifi.sap.interface", buffer.data(), nullptr) ==
-        0) {
-        return {};
-    }
-    return buffer.data();
 }
 
 std::string getP2pIfaceName() {
@@ -1688,18 +1678,19 @@ std::string WifiChip::allocateApOrStaIfaceName(uint32_t start_idx) {
     return {};
 }
 
-// AP iface names start with idx 1 for modes supporting
-// concurrent STA and not dual AP, else start with idx 0.
+// Return the first swlan (swlan0, swlan1) not already in use.
+// kApIfaceCnt depend on whether dual AP mode enabled.
 std::string WifiChip::allocateApIfaceName() {
-    // Check if we have a dedicated iface for AP.
-    std::string ifname = getApIfaceName();
-    if (!ifname.empty()) {
+    unsigned kApIfaceCnt = br_managed_ifaces_.empty() ? 1 : 2;
+    for (unsigned idx = 0; idx < kApIfaceCnt; ++idx) {
+        std::string ifname = "swlan" + std::to_string(idx);
+        if (findUsingName(ap_ifaces_, ifname)) continue;
+        if (findUsingName(sta_ifaces_, ifname)) continue;
+        if (findUsingNameFromBridge(ifname)) continue;
         return ifname;
     }
-    return allocateApOrStaIfaceName((isStaApConcurrencyAllowedInCurrentMode() &&
-                                     !isDualApAllowedInCurrentMode())
-                                        ? 1
-                                        : 0);
+    CHECK(false) << "All AP interfaces in use already!";
+    return {};
 }
 
 // Bridge interface used by AP.
