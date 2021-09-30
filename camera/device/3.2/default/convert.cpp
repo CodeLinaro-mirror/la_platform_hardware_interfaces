329871ace8738d16a1f3243cd3bf9f4e90a58f9f
Kalaivani R <kalr@@codeaurora.org>
2021-09-30 14:21:29 +0530

    Revert "Camera: Using PrivateInfo for HALStream and ChiStreamWrapper"
    This reverts commit 97474a42ab6ba6989914894b1c1007cf49aca0be.

--- camera/device/3.2/default/convert.cpp
+++ camera/device/3.2/default/convert.cpp
@@ -30,12 +30,6 @@ using ::android::hardware::graphics::common::V1_0::Dataspace;
 using ::android::hardware::graphics::common::V1_0::PixelFormat;
 using ::android::hardware::camera::device::V3_2::BufferUsageFlags;
 
-struct Camera3HalInfo
-{
-    HalStream* halStream;         ///< Pointer to the HAL stream
-    void*      hal3StreamPrvInfo; ///< A handle to private information for the HAL3Stream
-};
-
 bool convertFromHidl(const CameraMetadata &src, const camera_metadata_t** dst) {
     if (src.size() == 0) {
         // Special case for null metadata
@@ -97,15 +91,11 @@ void convertToHidl(const Camera3Stream* src, HalStream* dst) {
                 __FUNCTION__, src->stream_type);
     }
 
-    Camera3HalInfo* halInfo   = reinterpret_cast<Camera3HalInfo*>(src->priv);
-    HalStream*      halStream = NULL;
-
+    HalStream* halStream = NULL;
     if (src->reserved[0] != NULL) {
         halStream = (HalStream*)(src->reserved[0]);
     } else if (src->reserved[1] != NULL) {
         halStream = (HalStream*)(src->reserved[1]);
-    } else if (halInfo != NULL) {
-        halStream = halInfo->halStream;
     }
 
     // Check if overrideFormat is set and honor it
--- camera/device/3.3/default/convert.cpp
+++ camera/device/3.3/default/convert.cpp
@@ -30,12 +30,6 @@ using ::android::hardware::graphics::common::V1_0::Dataspace;
 using ::android::hardware::graphics::common::V1_0::PixelFormat;
 using ::android::hardware::camera::device::V3_2::BufferUsageFlags;
 
-struct Camera3HalInfo
-{
-    HalStream* halStream;         ///< Pointer to the HAL stream
-    void*      hal3StreamPrvInfo; ///< A handle to private information for the HAL3Stream
-};
-
 void convertToHidl(const Camera3Stream* src, HalStream* dst) {
     dst->overrideDataSpace = src->data_space;
     dst->v3_2.id = src->mId;
@@ -54,15 +48,11 @@ void convertToHidl(const Camera3Stream* src, HalStream* dst) {
                 __FUNCTION__, src->stream_type);
     }
 
-    Camera3HalInfo*  halInfo   = reinterpret_cast<Camera3HalInfo*>(src->priv);
-    HalStream*       halStream = NULL;
-
+    HalStream* halStream = NULL;
     if (src->reserved[0] != NULL) {
         halStream = (HalStream*)(src->reserved[0]);
     } else if (src->reserved[1] != NULL) {
         halStream = (HalStream*)(src->reserved[1]);
-    } else if (halInfo != NULL) {
-        halStream = halInfo->halStream;
     }
 
     if (halStream != NULL) {
