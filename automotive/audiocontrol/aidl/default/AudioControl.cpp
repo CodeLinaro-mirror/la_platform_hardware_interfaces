d41179298f3cab5a865929d5f169841e3d8a9fd3
Sunish Nambayil <quic_snambayi@quicinc.com>
2024-02-06 22:24:58 +0530

    Fix AOSP Compiler warning
    Suppress unused functions warning as errors.
    
    Change-Id: I969055184d3b3078e282d4267b4a9d931d96f908

--- automotive/audiocontrol/aidl/default/AudioControl.cpp
+++ automotive/audiocontrol/aidl/default/AudioControl.cpp
@@ -100,7 +100,7 @@ void fillProfile(const std::vector<int32_t>& channelLayouts,
     profile->sampleRates.insert(profile->sampleRates.end(), sampleRates.begin(), sampleRates.end());
 }
 
-AudioProfile createProfile(PcmType pcmType, const std::vector<int32_t>& channelLayouts,
+__attribute__((unused)) AudioProfile createProfile(PcmType pcmType, const std::vector<int32_t>& channelLayouts,
                            const std::vector<int32_t>& sampleRates) {
     AudioProfile profile;
     profile.format.type = AudioFormatType::PCM;
@@ -109,7 +109,7 @@ AudioProfile createProfile(PcmType pcmType, const std::vector<int32_t>& channelL
     return profile;
 }
 
-AudioProfile createProfile(const std::string& encodingType,
+__attribute__((unused)) AudioProfile createProfile(const std::string& encodingType,
                            const std::vector<int32_t>& channelLayouts,
                            const std::vector<int32_t>& sampleRates) {
     AudioProfile profile;
