b771444a43cdc1faa3c48c9f73c22ef9838ba48b
Yifei Zhao <quic_yifezhao@quicinc.com>
2024-09-11 11:02:42 +0800

    wifi-hal-rpc:Add wifi hal systemd services and ip config shell script to support wlan error recovery
    Change-Id: I7c42de27684aff510c42c23e3d4178f6f3e21640

---
+++ wifi/aidl/default/script/ip_config_wlan.sh
@@ -0,0 +1,75 @@
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+#!/bin/sh
+echo "ip_config"
+
+cmd=$1
+
+echo "ip_config: $cmd"
+PLATFORM=`uname -a| cut -d ' ' -f2`
+IP_ADDRESS=`cat /etc/someip/vsomeip_server.json | grep unicast |cut -d '"' -f4`
+WLAN_KO_PATH=`find /lib/modules/ -name "wlan-hamilton.ko"`
+
+insmod_ko()
+{
+    insmod $WLAN_KO_PATH
+    while true
+    do
+        result=$(ifconfig wlan0 2>&1)
+        if echo "$result" | grep -q "error fetching interface"; then
+            sleep 0.5
+        else
+            break
+        fi
+    done
+}
+
+config_vlan()
+{
+    echo $IP_ADDRESS
+        case "$PLATFORM" in
+        imx8*)
+            echo "config vlan"
+            insmod $WLAN_KO_PATH
+            ip link add link eth0 name vlan41 type vlan id 41
+            ip addr add 10.41.2.103/24 dev vlan41
+            ip link set vlan41 up
+            ip link add link eth0 name vlan43 type vlan id 43
+            ip link set vlan43 up
+            ;;
+        *)
+            echo "Unknown platform: $PLATFORM"
+            ;;
+    esac
+}
+
+config_ethernet()
+{
+    echo "ip_address:" $IP_ADDRESS
+        case "$PLATFORM" in
+        imx8*)
+            echo "config ethernet"
+            insmod $WLAN_KO_PATH
+            ;;
+        *)
+            echo "Unknown platform: $PLATFORM"
+            ;;
+    esac
+}
+
+case "$cmd" in
+    vlan)
+        insmod_ko
+        config_vlan
+        ;;
+    ethernet)
+        insmod_ko
+        config_ethernet
+        ;;
+    *)
+        echo "unknown para: $cmd"
+        exit 1
+        ;;
+esac
+exit 0
