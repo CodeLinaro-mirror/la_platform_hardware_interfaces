5d14f7c415f68279aa1369e26261a71233ea9a8b
Yanfei Zhou <zyanfei@qti.qualcomm.com>
2025-08-01 14:22:51 +0530

    A2DP_SRC: Support dual A2DP source HAL
    Extend the A2DP source HAL to support dual instances:
    > default: IBluetoothAudioProviderFactory::descriptor/default
    > new: IBluetoothAudioProviderFactory::descriptor/new
    
    CRs-Fixed: 4255290
    
    Change-Id: I69598d91234e577f634e4557ac1eb3d3e3e037cd

--- bluetooth/audio/aidl/default/service.cpp
+++ bluetooth/audio/aidl/default/service.cpp
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ *
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #define LOG_TAG "BtAudioAIDLService"
@@ -25,14 +30,32 @@
 using ::aidl::android::hardware::bluetooth::audio::
     BluetoothAudioProviderFactory;
 
-extern "C" __attribute__((visibility("default"))) binder_status_t
-createIBluetoothAudioProviderFactory() {
-  auto factory = ::ndk::SharedRefBase::make<BluetoothAudioProviderFactory>();
-  const std::string instance_name =
-      std::string() + BluetoothAudioProviderFactory::descriptor + "/default";
+static constexpr uint8_t kMaxA2dpConn = 2;
+
+static std::string getInstanceName(uint8_t index) {
+  return std::string() + BluetoothAudioProviderFactory::descriptor +
+         (index == 0 ? "/default" : "/new");
+}
+
+binder_status_t createIBluetoothAudioProviderFactoryInternal(uint8_t index) {
+  auto factory = ::ndk::SharedRefBase::make<BluetoothAudioProviderFactory>(index);
+  const std::string instance_name = getInstanceName(index);
+  ALOGW("%s: instance_name %s", __func__, instance_name.c_str());
   binder_status_t aidl_status = AServiceManager_addService(
       factory->asBinder().get(), instance_name.c_str());
   ALOGW_IF(aidl_status != STATUS_OK, "Could not register %s, status=%d",
            instance_name.c_str(), aidl_status);
   return aidl_status;
+}
+
+extern "C" __attribute__((visibility("default"))) binder_status_t
+createIBluetoothAudioProviderFactory() {
+  binder_status_t final_aidl_status = STATUS_OK;
+  for (uint8_t i = 0; i < kMaxA2dpConn; i++) {
+    binder_status_t status = createIBluetoothAudioProviderFactoryInternal(i);
+    if (final_aidl_status == STATUS_OK && status != STATUS_OK) {
+      final_aidl_status = status;
+    }
+  }
+  return final_aidl_status;
 }
