e7f3e4d3c5757e4a168611ba7618a992bb846292
Shaik Jabida <sjabida@qti.qualcomm.com>
2025-10-23 17:39:07 +0530

    audio : for avoiding noise issue
    To fix the noise issue for the USB headset,
    we propose a solution: Check if the value
    of byteswritten is less than zero only,
    if it equals zero it will enter a sleep state,
    and this is what causes the noise.
    
    Change-Id: I48c977676ad23fde12cdd8f421de1caeb0fb15bd
    Signed-off-by: Shaik Jabida <sjabida@qti.qualcomm.com>

--- audio/aidl/default/alsa/StreamAlsa.cpp
+++ audio/aidl/default/alsa/StreamAlsa.cpp
@@ -170,7 +170,7 @@ StreamAlsa::StreamAlsa(StreamContext* context, const Metadata& metadata, int rea
     } else {
         if (property_get_bool("vendor.audio.gaming.enabled", false /* default_value */)) {
             size_t bytesWritten = splitAndWriteAudioHapticsStream(buffer, frameCount, latencyMs);
-            if (bytesWritten <= 0) {
+            if (bytesWritten < 0) {
                 LOG(ERROR) << __func__ << ": write failed, ret: " << bytesWritten;
                 std::this_thread::sleep_for(std::chrono::milliseconds((frameCount * 1000) / 48000/* hardcoded device sample rate*/));
             }
